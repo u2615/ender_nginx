170dae71106ee49fc3b169c312bafc78b7383948
antoine <antoine.serveaux+dev@gmail.com>
2021-03-04 14:47:58 +0100

    Fixed sh not having bash like array syntax

--- src/set_permissions.sh
+++ src/set_permissions.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-needed_paths=(
-  /var/run/nginx.pid 
-  /usr/share/nginx 
-  /etc/nginx 
-  /var/log/nginx 
-  /usr/lib/nginx 
-  /var/cache/nginx
-)
+needed_paths="/var/run/nginx.pid /usr/share/nginx /etc/nginx /var/log/nginx /usr/lib/nginx /var/cache/nginx"
 
-for file in ${needed_paths[@]}; do
+for file in ${needed_paths}; do
   echo "Setting permission for $file"
   chown -R root:nginx $file
   find $file -type f -exec chmod 660 '{}' ';'
